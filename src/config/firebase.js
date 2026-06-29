66a83e5fec8ff8326e6472a1cc65512fcfee5288
Eshan <eshangunsekara@gmail.com>
2026-06-29 14:09:55 +0530

    chore: secure firebase api keys using environment variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_EIPZVPhY-RHJLl49hCKwGxvykv1ouSY",
-  authDomain: "my-portfolio-946a9.firebaseapp.com",
-  projectId: "my-portfolio-946a9",
-  storageBucket: "my-portfolio-946a9.firebasestorage.app",
-  messagingSenderId: "258358649196",
-  appId: "1:258358649196:web:f7dcf6c63dafb532645349",
-  measurementId: "G-ZQJ5LFQFN1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
